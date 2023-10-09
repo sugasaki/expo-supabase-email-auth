734efbb287ace88ed8dfc27d9328f1362b31ba69
sugasaki <sugasaki@gmail.com>
2023-10-09 18:27:43 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # expo-supabase-email-auth
+
+https://supabase.com/docs/guides/getting-started/tutorials/with-expo-react-native?database-method=dashboard&auth-store=async-storage
---
+++ lib/supabase.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASEURL ?? ''
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANONKEY ?? ''
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
